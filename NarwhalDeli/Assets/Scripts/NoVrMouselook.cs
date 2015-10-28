c23dbac9c24ad9a5842961dc6e697163cd17cb5b
John Conover <111jbcon@gmail.com>
2015-10-27 20:08:28 -0400

    Added script in case Oculus not connected

---
+++ NarwhalDeli/Assets/Scripts/NoVrMouselook.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+using System.Collections;
+
+public class NoVrMouselook : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+	    //check for oculus
+        if (!VRDevice.isPresent)
+        {
+            Debug.Log("Detected " + VRSettings.loadedDevice);
+        }
+        else
+        {
+            Debug.Log("No VR device detected.  Initializing First Person Controller...");
+            transform.gameObject.AddComponent<SimpleSmoothMouseLook>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
