ed1dc97d057437ea77d3e2c8b9d1ffd8cd1a1961
Ben Chang <changb3@rpi.edu>
2015-11-13 16:40:08 -0500

    Initial CAVE commit

---
+++ NarwhalDeli/Assets/CAVE/Scripts/Movement/CharControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharControl : MonoBehaviour {
+
+	public float maxSpeed;
+	private Vector2 movement;
+
+	private Transform pointer;
+
+	// Use this for initialization
+	void Start () {
+		pointer = GameObject.Find ("PointerRoot").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement = new Vector3(Input.GetAxis ("Horizontal"), Input.GetAxis("Vertical"));
+	}
+
+	void FixedUpdate(){
+		float pointerYRot = pointer.rotation.eulerAngles.y;
+
+		transform.position += new Vector3( maxSpeed * movement.x * Time.deltaTime, 0f,
+		                                   maxSpeed * movement.y * Time.deltaTime);
+	}
+
+}
