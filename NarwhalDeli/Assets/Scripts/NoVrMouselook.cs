dccbb5378eb3f1be09201c918d973e8fbf888253
jbcon <111jbcon@gmail.com>
2015-11-13 17:23:48 -0500

    Fixed wasd+mouse control, stacking height

--- NarwhalDeli/Assets/Scripts/NoVrMouselook.cs
+++ NarwhalDeli/Assets/Scripts/NoVrMouselook.cs
@@ -4,6 +4,12 @@ using System.Collections;
 
 public class NoVrMouselook : MonoBehaviour {
 
+    public float xMovement = 2;
+    public float zMovement = 2;
+    public float snapSpeed = 2;
+
+    private Vector3 initialPos;
+
 	// Use this for initialization
 	void Awake () {
 	    //check for oculus
@@ -15,11 +21,20 @@ public class NoVrMouselook : MonoBehaviour {
         {
             Debug.Log("No VR device detected.  Initializing First Person Controller...");
             transform.gameObject.AddComponent<SimpleSmoothMouseLook>();
+            initialPos = transform.position;
+            Camera.main.fov = 106;
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if (!VRDevice.isPresent)
+        {
+            float movX = Input.GetAxisRaw("Horizontal") * xMovement;
+            float movZ = Input.GetAxisRaw("Vertical") * zMovement;
+            Quaternion yRot = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+
+            transform.position = Vector3.Slerp(transform.position,initialPos + yRot * new Vector3(movX, 0, movZ ), Time.deltaTime * snapSpeed);
+        }
 	}
 }
