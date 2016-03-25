81a35671136d58bc08af0d64a8e519f511bacf7b
jbcon <111jbcon@gmail.com>
2016-03-25 17:14:33 -0400

    Flipper-based swatting mechanics

---
+++ NarwhalDeli/Assets/Scripts/Flipper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flipper : MonoBehaviour {
+
+    public GameObject flipperBase;
+
+    private SteamVR_TrackedObject myFlipper;
+    private GameObject flipperObject;
+    private static int numControllers = 0;
+
+	// Use this for initialization
+	void Awake () {
+        flipperBase = Instantiate(flipperBase);
+        myFlipper = flipperBase.GetComponent<SteamVR_TrackedObject>();
+        myFlipper.origin = gameObject.transform;
+        myFlipper.SetDeviceIndex(++numControllers);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
