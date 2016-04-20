201a51f7b23d300e7dc9c23f076986cb43e0d634
John Conover <111jbcon@gmail.com>
2016-04-20 03:06:53 -0400

    sign perfected

---
+++ NarwhalDeli/Assets/Scripts/DONT LOOK, IM HIDEOUS!!/TitleBoardString.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TitleBoardString : MonoBehaviour {
+
+    public Transform[] anchors;
+    public Color ropeColor;
+    public Material ropeMat;
+    private GameObject horn;
+    LineRenderer r;
+
+    // Use this for initialization
+    void Awake() {
+        horn = GetComponent<CharacterJoint>().connectedBody.gameObject;
+        Vector3 hornAnchor = GetComponent<CharacterJoint>().connectedAnchor;
+        r = gameObject.AddComponent<LineRenderer>();
+        r.SetVertexCount(3);
+        r.material = ropeMat;
+        r.SetColors(ropeColor, ropeColor);
+        r.SetPosition(0, anchors[0].position);
+        r.SetPosition(1, anchors[1].position);
+        r.SetPosition(2, anchors[2].position);
+        r.SetWidth(0.1f,0.1f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 hornAnchor = horn.transform.InverseTransformPoint(GetComponent<CharacterJoint>().connectedAnchor);
+        r.SetPosition(0, anchors[0].position);
+        r.SetPosition(1, anchors[1].position);
+        r.SetPosition(2, anchors[2].position);
+    }
+}
