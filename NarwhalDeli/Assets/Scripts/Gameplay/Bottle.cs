10693e1863e80de7673c1a3246997962447b5612
jbcon <111jbcon@gmail.com>
2016-04-29 17:06:47 -0400

    Spray ketchup from the controllers

---
+++ NarwhalDeli/Assets/Scripts/Gameplay/Bottle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Valve.VR;
+using System.Collections;
+
+public class Bottle : MonoBehaviour {
+
+    SteamVR_TrackedObject controller;
+    int controllerIndex;
+    public GameObject proj;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponentInParent<SteamVR_TrackedObject>();
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)controller.index);
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            GameObject p = Instantiate(proj, transform.position, Quaternion.identity) as GameObject;
+            p.GetComponent<Rigidbody>().AddForce(transform.forward * 30, ForceMode.Impulse);
+            Destroy(p, 2);
+        }
+        else
+        {
+
+        }
+    }
+}
