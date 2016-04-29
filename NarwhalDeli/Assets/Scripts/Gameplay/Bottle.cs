81ea72153162e9e5788ff3ca6fec3ecf8f8c5730
jbcon <111jbcon@gmail.com>
2016-04-29 17:39:34 -0400

    Actual ketchup and mustard bottles!

--- NarwhalDeli/Assets/Scripts/Gameplay/Bottle.cs
+++ NarwhalDeli/Assets/Scripts/Gameplay/Bottle.cs
@@ -11,16 +11,15 @@ public class Bottle : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         controller = GetComponentInParent<SteamVR_TrackedObject>();
-        
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
         SteamVR_Controller.Device device = SteamVR_Controller.Input((int)controller.index);
         if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
         {
-            GameObject p = Instantiate(proj, transform.position, Quaternion.identity) as GameObject;
-            p.GetComponent<Rigidbody>().AddForce(transform.forward * 30, ForceMode.Impulse);
+            GameObject p = Instantiate(proj, transform.position + transform.forward / 4, transform.rotation) as GameObject;
+            p.GetComponent<Rigidbody>().AddForce(transform.forward * 20, ForceMode.Impulse);
             Destroy(p, 2);
         }
         else
